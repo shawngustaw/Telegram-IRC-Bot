381ad85b82606d5cd6588594ee530def779fe9ce
Shawn Gustaw <srgustaw@gmail.com>
2016-07-29 00:40:57 -0400

    Move most code into goroutines/channels

--- main.go
+++ main.go
@@ -10,14 +10,16 @@ import (
 )
 
 type ircBot struct {
-	Nickname      string // The nick the bot will use
-	Username      string // The IRC username the bot will use
-	Password      string // Server password
-	Host          string // The IRC Server hostname
-	Port          int    // The IRC Server port
-	UseTLS        bool   // Should we connect using TLS?
-	TLSServerName string // Must supply if above is true
-	Debug         bool   // Log all IRC comms to std out
+	ircConn       *irc.Connection
+	Nickname      string       // The nick the bot will use
+	Username      string       // The IRC username the bot will use
+	Password      string       // Server password
+	Host          string       // The IRC Server hostname
+	Port          int          // The IRC Server port
+	UseTLS        bool         // Should we connect using TLS?
+	TLSServerName string       // Must supply if above is true
+	Debug         bool         // Log all IRC comms to std out
+	Channel       chan message // Communication back to main thread
 }
 
 type telegramBot struct {
@@ -63,39 +65,151 @@ func onPrivateMessage(event *irc.Event) {
 	}(event)
 
 }
-
-func setUpIRCConnection() <-chan message {
-	channel := make(chan message)
+func setUpTelegramConnection(
+	readChannel chan string,
+	writeChannel chan string,
+	ircReadChannel chan string,
+	ircWriteChannel chan string) {
 
 	go func() {
-		ircConn = irc.IRC("Abot", "Abot")
-		ircConn.VerboseCallbackHandler = true
+		fmt.Print("\n\n\n")
+		fmt.Print("SETTING UP THE TELEGRAM CONNECTION")
+		fmt.Print("\n\n\n")
 
-		ircConn.Connect("irc.oftc.net:6667")
+		TOKEN := ""
+		bot, e := tgbotapi.NewBotAPI(TOKEN)
 
-		ircConn.AddCallback("001", onWelcome)
-		ircConn.AddCallback("PRIVMSG", onPrivateMessage)
+		if e != nil {
+			log.Panic(e)
+		}
 
-		ircConn.Loop()
+		bot.Debug = true
+
+		log.Printf("Authorized on account %s", bot.Self.UserName)
+
+		u := tgbotapi.NewUpdate(0)
+
+		u.Timeout = 60
+
+		updates, error := bot.GetUpdatesChan(u)
+
+		if error != nil {
+			log.Panic(error)
+		}
+
+		// read from channel
+		go func() {
+			for {
+				fmt.Print("Writing to Telegram channel")
+				message := <-writeChannel
+				fmt.Print(message)
+				msg := tgbotapi.NewMessage(1, message)
+				bot.Send(msg)
+
+			}
+		}()
+
+		for update := range updates {
+			if update.Message == nil {
+				continue
+			}
+
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			go func() {
+				fmt.Print("Writing to IRC Channel")
+				ircWriteChannel <- update.Message.Text
+			}()
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+
 	}()
+
+}
+
+func setUpIRCConnection(
+	readChannel chan string,
+	writeChannel chan string,
+	telegramReadChannel chan string,
+	telegramWriteChannel chan string) {
+
+	fmt.Print("\n\n\n\n")
+	fmt.Print("setting up the connection to irc")
+
+	ircConn = irc.IRC("Abbbot", "Abbbot")
+	ircConn.VerboseCallbackHandler = true
+
+	ircConn.Connect("irc.oftc.net:6667")
+	//ircConn.Connect("chat.freenode.net:6667")
+
+	fmt.Print("setting up the callbacks")
+	ircConn.AddCallback("001", func(e *irc.Event) {
+
+		ircConn.Join("#tag")
+		time.Sleep(100 * time.Millisecond)
+
+		ircConn.SendRawf("%s %s :%s", "PRIVMSG", "#tag", "TorontoCryptoBot has joined the channel.")
+		return
+	})
+
+	ircConn.AddCallback("PRIVMSG", func(event *irc.Event) {
+		fmt.Print(event.Nick)         // contains the sender
+		fmt.Print(event.Message())    //contains the message
+		fmt.Print(event.Arguments[0]) // contains the channel
+
+		go func() {
+			fmt.Print("\n\n\n")
+			fmt.Print("WRITING TO TELEGRAM CHANNEL")
+			telegramWriteChannel <- event.Message()
+		}()
+
+	})
+
+	// read from channel
+	go func() {
+		for {
+			fmt.Print("Reading from IRC channel")
+			message := <-readChannel
+			fmt.Print(message)
+
+		}
+	}()
+
+	fmt.Print("Entering the infinite loop for irc events")
+	fmt.Print("\n\n\n\n")
+
+	ircConn.Loop()
 
 	//ircConn.UseTLS = ircBot.UseTLS
 	//ircConn.TLSConfig = &tls.Config{
 	//	ServerName: ircBot.Host,
 	//}
 
-	ircConn.VerboseCallbackHandler = ircBot.Debug
-	return channel
+	//ircConn.VerboseCallbackHandler = ircBot.Debug
 
 }
 
 func main() {
-	setUpIRCConnection()
+	//	telegramChannel := setUpTelegram()  // TODO: create this channel, pass it to IRC connection
+	ircWriteChannel := make(chan string)
+	ircReadChannel := make(chan string)
+	telegramWriteChannel := make(chan string)
+	telegramReadChannel := make(chan string)
 
-	x := 0
+	go func() {
+		setUpIRCConnection(ircReadChannel, ircWriteChannel, telegramReadChannel, telegramWriteChannel)
+	}()
+
+	go func() {
+		setUpTelegramConnection(telegramReadChannel, telegramWriteChannel, ircReadChannel, ircWriteChannel)
+	}()
 
 	for {
-		x += 0
+		// keep main program running
 	}
 
 	return // BEGIN TELEGRAM CODE BELOW
